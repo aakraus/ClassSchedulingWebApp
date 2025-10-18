2c9b044656014c4e4fe84959f8251bcd3f5c47d6
Aaron Kraus <aaron.kraus38@gmail.com>
2025-10-17 23:14:48 -0700

    complete skeleton for parse.js

--- JS/parse.js
+++ JS/parse.js
@@ -26,5 +26,53 @@ export function parseMeetings(cell) {
 }
 
 export async function loadCSV(text) {
-    // accept CSV and normalize data
+  // accept CSV and normalize data
+
+  const rows = d3.csvParse(text);
+
+  const out = []; // normalized rows used by apps
+  const skipped = []; // reasons for rows that can't be parsed
+  const deptsSet = new Set(); // stores unique departments to populate datalist
+
+  for (let i = 0; i < rows.length; i++) {
+    const r = rows[i];
+
+    const mtgs = parseAllMeetings(r["Day/Time/Location"]); // parse values for day, time, and location contained in chunks
+
+    if (!mtgs.length) {
+      // inform user of potential CSV formatting error to fix if needed
+      skipped.push('Row ${i + 1}: unable to parse Day/Time/Location -> "${r["Day/Time/Location"] ?? ""}"');
+      continue;
+    }
+
+    // gather all other fields of note
+    const enrolled = Number(r["Enrollment"]) || 0; // for weighting results by enrollment
+    const dept = (r["Department"] || "").trim(); // for filtering results by department
+    if (dept) deptsSet.add(dept); // build datalist
+    const title = r["Event Long Name"] || ""; // pull title from CSV
+    const course_id = r["Event ID"] || ""; // pull event ID from CSV
+    
+    // expand each meeting chunk into one row per day (i.e., Tue/Thu becomes 2 rows)
+    for (const mtg of mtgs) {
+      for (const d of mtg.days) {
+        out.push({
+          course_id,
+          title,
+          dept,
+          day: d,                                            
+          start: toMinutes(mtg.start_time),                  
+          end:   toMinutes(mtg.end_time),
+          location: mtg.location || "",
+          enrolled                                           
+        });
+      }
+    }
+  }
+
+  // return normalized data, any parse warnings, and sorted department list
+  return {
+    rows: out,
+    skipped,
+    depts: Array.from(deptsSet).sort()
+  };
 }
